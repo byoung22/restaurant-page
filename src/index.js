2127ac3235db1326d21239b0295a255bbc1f1708
Bailey <baileywyoung@gmail.com>
2023-05-11 16:30:01 -0700

    Added logos and css module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+import _ from 'lodash';
+import './style.css';
+
+import Logo from './Photos/logo.png';
+const myLogo = new Image();
+myLogo.src = Logo;
+
 const content = document.createElement('div');
 content.classList.add('content');
 document.body.appendChild(content);
-console.log('hi');
+
+content.appendChild(myLogo);
+//#0C50B9
